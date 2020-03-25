f5f336b9317128f909873005ad0a1c465f06a822
hassan <hassankhaled010@gmail.com>
2020-03-25 07:48:40 +0200

    inital commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express=require('express');
+const hbs=require('hbs');
+const app=express();
+const fs=require('fs');
+app.use(express.static(__dirname + '/New folder'));
+hbs.registerPartials(__dirname +'/views/partials')
+
+app.set('view engine','hbs')
+hbs.registerHelper('hassan',()=>{
+    return ( new Date().getFullYear()+'hassan')
+})
+
+hbs.registerHelper('kabar',(text)=>{
+return text.toUpperCase()
+})
+
+app.use((req,res,next)=>{
+
+    let now=new Date().toString();
+  const log=`${now}: ${req.method} ==${req.url}`;
+  fs.appendFileSync('log',log);
+  console.log(log)
+  
+    next();
+
+})
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/',(req,res)=>{
+
+    res.render('home.hbs',{
+        welcome:'welcom to our site',
+        
+    })
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        message:'bad page'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pagetitle:'aboutpage',
+        
+    })
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log('page on port 3000')
+});
